b965fb21c39fe51c9b8f0d16a860d0d996ac7a19
caio <caio_mello@id.uff.br>
2017-09-04 13:46:29 -0300

    converte entrada para complexo

--- cpptut.cpp
+++ cpptut.cpp
@@ -14,10 +14,22 @@ int main(int argc, char *argv[]){
 	ifstream infile;
 	infile.open(argv[1], ios::binary | ios::in);
 	int i = 0;
-
+	int count = 0;
+	short int real_temp = 0;
+	short int imag_temp = 0;
 	while(infile.peek() != EOF){
 		infile.read((char *) (& x), 2);
-		infile.seekg(++i);
+		i=i+2;
+		infile.seekg(i);
+		if(count==0){
+			real_temp = x;
+			count++;
+		}
+		else{
+			imag_temp = x;
+			complex<int> complexo(real_temp,imag_temp);
+			count=0;
+		}
 	}
 	infile.clear();
 	infile.close();
